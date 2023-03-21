1492f9b7727dbf77dcb7af7a4d3054bde34a13ab
Manish Bajpai <www.manishbajpai055@gmail.com>
2023-03-21 17:03:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ exports.validateEmail = (email) => {
   return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 };
 
-app.all('/', SendMails)
+app.all('/mailto', SendMails)
 
 
 app.listen(process.env.PORT || 3000)
