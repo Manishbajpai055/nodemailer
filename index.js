3f442006eb1fb9a5f266030a79e076a8d3c3a92a
manishbajpai055 <manishbajpai055@gmail.com>
2023-03-23 15:57:43 +0530

    node mailer

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
 
 const _ = require('lodash');
 const nodemailer = require('nodemailer')
+const router = express.Router();
 
-const SendMails = (req,res) => {
-    let {emails, Subject, body} = req.body
+var path = require('path');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(cors({ credentials: true, Origin: '*' }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+app.use('/', router)
+
+
+app.listen(process.env.PORT || 5501)
+
+
+
+const SendMails = async (req, res) => {
+
+
+  console.log("🚀 ~ file: index.js:9 ~ SendMails ~ emails, Subject, body:", req.body)
+  // return
+  let { emails, Subject, body } = req.body
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     port: 465,
@@ -41,7 +73,7 @@ const SendMails = (req,res) => {
       from: 'gyandas12998@gmail.com',
       to: emails,
       subject: Subject,
-      html:body
+      html: body
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
@@ -49,6 +81,7 @@ const SendMails = (req,res) => {
         console.log(error);
       } else {
         console.log('Email sent: ' + info.response);
+        res.status(200).send({ "message": info.response })
       }
     });
   }
@@ -59,7 +92,7 @@ exports.validateEmail = (email) => {
   return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 };
 
-app.all('/mailto', SendMails)
 
+module.exports = SendMails
 
-app.listen(process.env.PORT || 3000)
+router.post('/mailto', SendMails)
